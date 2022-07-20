2d8217a8038363825f6d54b34c650c847aa5941e
Roberto R <roberto@fxdigital.uk>
2022-07-21 01:54:22 +0200

    chore: clean unused resources, set settings properly, add favicon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import { Launch } from '@lightningjs/sdk'
-import App from './App.js'
+import { Launch } from "@lightningjs/sdk";
+import App from "./App.js";
 
-export default function() {
-  return Launch(App, ...arguments)
+function setFavicon() {
+  try {
+    document.querySelector("link[rel*='icon']").href = "static/favicon.ico";
+  } catch (exception) {
+    console.log(
+      "I don't really want to deal with this exception, but here it is:",
+      exception
+    );
+  }
+}
+
+export default function () {
+  setFavicon();
+  return Launch(App, ...arguments);
 }
