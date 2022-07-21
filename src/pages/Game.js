bc78378608ab9fb6070edfd152b8ea492f33a350
Roberto R <roberto@fxdigital.uk>
2022-07-21 02:57:03 +0200

    feat: setup game loop

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,41 @@
-import { Lightning } from "@lightningjs/sdk";
+import { Lightning, Colors } from "@lightningjs/sdk";
 
 class Game extends Lightning.Component {
   static _template() {
     return {
       w: 1920,
       h: 1080,
-      color: 0xff333333,
+      color: Colors("black").get(),
       rect: true,
     };
+  }
+
+  enableGameLoop() {
+    requestAnimationFrame(this.gameLoop);
+  }
+
+  gameLoop(currentTimestamp) {
+    const deltaTime = Math.ceil(currentTimestamp - this.previousTimeStamp);
+    this.deltaTime = deltaTime;
+    this.previousTimeStamp = currentTimestamp;
+    requestAnimationFrame(this.gameLoop);
+  }
+
+  disableGameLoop() {
+    cancelAnimationFrame(this.gameLoop);
+  }
+
+  _active() {
+    this.enableGameLoop();
+  }
+
+  _inactive() {
+    this.disableGameLoop();
+  }
+
+  _construct() {
+    this.gameLoop = this.gameLoop.bind(this);
+    this.previousTimeStamp = 0;
   }
 }
 
